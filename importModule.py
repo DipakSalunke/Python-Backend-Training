9075340881bd39b4976b286847212ba77f18990b
DipakSalunke <dipakbs810@gmail.com>
2021-02-18 00:31:27 +0530

    Day_3 Tasks

---
+++ importModule.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 17 13:51:10 2021
+
+@author: dipak
+"""
+
+def imp():
+    print("I'm being imported")
+def imp_and_rename():
+    print("I'm being imported and renamed")
+if __name__ == '__main__':
+    print("I'm Running")
