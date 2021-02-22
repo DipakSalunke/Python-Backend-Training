74b55742953806b462c39c792f262c5c41d3ad54
DipakSalunke <dipakbs810@gmail.com>
2021-02-22 22:50:52 +0530

    modified task defn

--- day_6/app.py
+++ day_6/app.py
@@ -1,9 +1,11 @@
 '''
-Implement a small Flask App with HTML
+Implement a small Flask App for Groceries API
 Create a new project
 Create and activate venv
 Install Flask
-Use HTML templates to display a developers name in a color of your choice
+Develop below endpoints:
+/groceries - display all the veggies
+/groceries/carrot - display the name of hte veggie and the current qty
 Implement POST and DELETE as well for the Groceries API to add/delete veggies
 '''
 from flask import Flask, jsonify, request
